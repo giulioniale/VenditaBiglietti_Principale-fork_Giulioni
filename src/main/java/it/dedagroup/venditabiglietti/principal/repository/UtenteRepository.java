255665746d649e33cd7a657e02bc56eaef0cd3ef
BRNSMN99H <BRNSMN99H@3J5FRV3.nco.inet>
2023-11-15 16:39:10 +0100

    AGGIUNTA REPOSITORY DELLA CLASSE UTENTE

---
+++ src/main/java/it/dedagroup/venditabiglietti/principal/repository/UtenteRepository.java
@@ -0,0 +1,21 @@
+package it.dedagroup.venditabiglietti.principal.repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import it.dedagroup.venditabiglietti.principal.model.Ruolo;
+import it.dedagroup.venditabiglietti.principal.model.Utente;
+
+
+public interface UtenteRepository extends JpaRepository<Utente, Long>{
+	
+	public Optional<Utente> findByEmailAndPassword(String email, String password);
+	public Optional<Utente> findByTelefono(String telefono);
+	public Optional<Utente> findByDataDiNascita(LocalDate dataDiNascita);
+	public Optional<Utente> findByNomeAndCognome(String nome, String cognome);
+	public Optional<Utente> findByRuolo(Ruolo ruolo);
+	public Optional<Utente> findByNomeAndCognomeAndIsAvailableTrue(String nome, String cognome);
+
+}
