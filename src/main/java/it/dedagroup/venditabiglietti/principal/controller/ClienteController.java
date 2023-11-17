b349f37ac1dc18db857e6a3c62a7abd5a0919c66
BGNMTT02L <matteo.bagnato@dedagroup.it>
2023-11-17 16:48:25 +0100

    implementato metodo disattivaUtente nel controller ClienteController.java

--- src/main/java/it/dedagroup/venditabiglietti/principal/controller/ClienteController.java
+++ src/main/java/it/dedagroup/venditabiglietti/principal/controller/ClienteController.java
@@ -1,8 +1,36 @@
 package it.dedagroup.venditabiglietti.principal.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import it.dedagroup.venditabiglietti.principal.facade.ClienteFacade;
+import it.dedagroup.venditabiglietti.principal.model.Utente;
+import it.dedagroup.venditabiglietti.principal.repository.UtenteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
+@RequestMapping("/cliente")
+@AllArgsConstructor
 public class ClienteController {
 
+
+    private final ClienteFacade clienteFacade;
+
+    @PutMapping("/disattiva")
+    public ResponseEntity<String> disattivaUtente(Utente utente){
+        clienteFacade.disattivaUtente(utente);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Hai disattivato l'account di: " + utente.getNome());
+    }
+
+    @PutMapping("/modifica")
+    public ResponseEntity<String> modificaUtente(Utente utente){
+        clienteFacade.modificaUtente(utente);
+        return ResponseEntity
+                //TODO da completare
+    }
 }
--- src/main/java/it/dedagroup/venditabiglietti/principal/facade/ClienteFacade.java
+++ src/main/java/it/dedagroup/venditabiglietti/principal/facade/ClienteFacade.java
@@ -1,7 +1,20 @@
 package it.dedagroup.venditabiglietti.principal.facade;
 
+import it.dedagroup.venditabiglietti.principal.model.Utente;
+import it.dedagroup.venditabiglietti.principal.repository.UtenteRepository;
+import it.dedagroup.venditabiglietti.principal.serviceimpl.UtenteServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteFacade {
+    @Autowired
+    private UtenteServiceImpl service;
+
+    public void disattivaUtente(Utente utente){
+        service.eliminaUtente(utente.getId());
+    }
+
+    public void modificaUtente(Utente utente) {
+    }
 }
