59347e417a83dbcd16b95b8c0ce8982ae9ac8eaf
VLPGMR02B <VLPGMR02B@8N8FRV3.nco.inet>
2023-11-21 15:58:58 +0100

    Merge branch 'master' of https://github.com/GianmarcoVolpe/VenditaBiglietti_Principale_DreamTeam_GianmarcoVolpe.git

--- src/main/java/it/dedagroup/venditabiglietti/principal/serviceimpl/EventoServiceImpl.java
+++ src/main/java/it/dedagroup/venditabiglietti/principal/serviceimpl/EventoServiceImpl.java
@@ -1,34 +1,25 @@
 package it.dedagroup.venditabiglietti.principal.serviceimpl;
 
-<<<<<<< HEAD
+
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import it.dedagroup.venditabiglietti.principal.model.Evento;
 import it.dedagroup.venditabiglietti.principal.service.EventoServiceDef;
 import it.dedagroup.venditabiglietti.principal.service.GeneralCallService;
 
 @Service
 public class EventoServiceImpl implements EventoServiceDef, GeneralCallService{
 	
-	String pathEvento="http://localhost:8081";
 
 	@Override
 	public void eliminaEvento(long id) {
-		callPost(pathEvento+"manifestazione/delete/"+id, null, id, String.class);
+		callPost(servicePath+"delete/"+id, null, id, String.class);
 		
 	}
 
-=======
-import it.dedagroup.venditabiglietti.principal.model.Evento;
-import it.dedagroup.venditabiglietti.principal.service.EventoServiceDef;
-import it.dedagroup.venditabiglietti.principal.service.GeneralCallService;
-import jdk.jfr.Event;
-import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Service
-public class EventoServiceImpl implements GeneralCallService, EventoServiceDef {
 
     private String servicePath="http://localhost:8081/evento/";
 
@@ -38,5 +29,5 @@ public class EventoServiceImpl implements GeneralCallService, EventoServiceDef {
         Evento[] listaEventiFuturi =callGet(mioPath,null,null, Evento[].class);
         return List.of(listaEventiFuturi);
     }
->>>>>>> 88087fb054f8f6e35b3b9d94775a2abf72058f13
+
 }
